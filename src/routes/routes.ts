3316ccb5f4d4b6b3fb77acd6e93c179b851c2d24
Fedorich <panchev_fedor@mail.ru>
2025-05-20 21:08:44 +0300

    QuestionPage/Pagination

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router';
 import { MainLayout } from '@components/MainLayout/MainLayout';
 import { QuestionsPage } from '@/pades/QuestionsPage';
 import { NotFound } from '@/pades/NotFound';
+import { QuestionPage } from '@/pades/QuestionPage';
 
 export const router = createBrowserRouter([
 	{
@@ -9,6 +10,7 @@ export const router = createBrowserRouter([
 		Component: MainLayout,
 		children: [
 			{index: true, Component: QuestionsPage},
+			{path: '/question/:id', Component: QuestionPage},
 			{path: '*', Component: NotFound}
 		]
 	}
--- src/services/questions.ts
+++ src/services/questions.ts
@@ -13,11 +13,66 @@ export const questionApi = baseApi.injectEndpoints({
 					]
 					: [{ type: 'Questions', id: 'ALL' }],
 		}),
+		getQuestionById: builder.query<Question, { id: string | undefined }>({
+			query: ({ id }) => `questions/public-questions/${id}`,
+		// 	providesTags: (result) =>
+		// 		result
+		// 			? [
+		// 				...result.data.map(({ id }) => ({ type: 'Questions' as const, id })),
+		// 				{ type: 'Questions', id: 'ALL' },
+		// 			]
+		// 			: [{ type: 'Questions', id: 'ALL' }],
+		}),
 	}),
 });
 
-export const { useGetQuestionsQuery } = questionApi;
+export const { useGetQuestionsQuery, useGetQuestionByIdQuery } = questionApi;
 
 export const questionReducer = questionApi.reducer
+
+export interface User {
+	id: string;
+	username: string;
+}
+
+export interface Specialization {
+	id: number;
+	title: string;
+	description: string;
+	imageSrc: string;
+	createdAt: string;
+	updatedAt: string;
+}
+
+export interface Skill {
+	id: number;
+	title: string;
+	description: string;
+	imageSrc: string;
+	createdAt: string;
+	updatedAt: string;
+}
+
+export interface Question {
+	id: number;
+	title: string;
+	description: string;
+	code: string;
+	imageSrc: string;
+	keywords: string[];
+	longAnswer: string;
+	shortAnswer: string;
+	status: "public" | "private" | "draft"; // можно расширить, если есть другие значения
+	complexity: number;
+	rate: number;
+	createdById: string;
+	updatedById: string;
+	createdBy: User;
+	updatedBy: User;
+	questionSpecializations: Specialization[];
+	questionSkills: Skill[];
+	createdAt: string;
+	updatedAt: string;
+}
 
 
